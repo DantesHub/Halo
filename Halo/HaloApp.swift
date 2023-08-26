00d6c4ffc53ee22292ccd0862f6565348ec4c11e
Dante Kim <51376601+DantesHub@users.noreply.github.com>
2023-08-26 17:51:00 -0400

    Initial Commit

---
+++ Halo/HaloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaloApp.swift
+//  Halo
+//
+//  Created by Dante Kim on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct HaloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
